e9ef2a0bc8adc394f58d7fa7e87a87aedaa0d4a2
Sanghyuk Park <dropthegit@gmail.com>
2023-12-07 15:31:22 +0900

    Home_List

--- src/main/java/com/ll/medium/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/medium/domain/home/home/controller/HomeController.java
@@ -1,15 +1,24 @@
 package com.ll.medium.domain.home.home.controller;
 
+import com.ll.medium.domain.article.entity.Article;
+import com.ll.medium.domain.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+    private final ArticleService articleService;
+
     @GetMapping("/")
-    public String showMain(){
-        return "domain/home/home/main";
+    public String showHome(Model model){
+        List<Article> articles = this.articleService.getList();
+        model.addAttribute("articleList", articles);
+        return "home_list";
     }
 
 }
