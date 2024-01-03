f3d9e53875f0fc5f5dd380c1c20743072e043731
Sanghyuk Park <dropthegit@gmail.com>
2024-01-03 15:09:39 +0900

    필수미션3 유료데이터 100개씩 생성

--- src/main/java/com/ll/medium/global/initData/NotProd.java
+++ src/main/java/com/ll/medium/global/initData/NotProd.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 
+import java.util.stream.IntStream;
+
 @Configuration
 @RequiredArgsConstructor
 public class NotProd {
@@ -34,12 +36,19 @@ public class NotProd {
 
                 Member memberUser4 = memberService.join("user4", "1234");
 
-                articleService.write("제목 1", "내용 1", true, true, memberUser1);
-                articleService.write("제목 2", "내용 2", true, true, memberUser1);
-                articleService.write("제목 3", "내용 3", true, true, memberUser1);
-                articleService.write("제목 4", "내용 4", true, false, memberUser1);
-                articleService.write("제목 5", "내용 5", true, false, memberUser1);
+                IntStream.rangeClosed(5, 100).forEach(i -> {
+                    memberService.join("user " + i, "1234", true);
+                });
 
+                articleService.write("제목 1", "내용 1", true, true, memberUser1);
+                articleService.write("제목 2", "내용 2", true, false, memberUser1);
+                articleService.write("제목 3", "내용 3", true, false, memberUser1);
+                articleService.write("제목 4", "내용 4", true, false, memberUser3);
+                articleService.write("제목 5", "내용 5", true, false, memberUser4);
+
+                IntStream.rangeClosed(6, 100).forEach(i -> {
+                    articleService.write("제목 " + i, "내용 " + i, true, true, memberUser2);
+                });
 
             }
         };
